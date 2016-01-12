14fbe67c87217687a40b20432a58b73dc6107f42
Soutaro Matsumoto <matsumoto@soutaro.com>
2016-01-13 02:12:12 +0900

    Revise UA Test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
   def request_from_ios_device?
-    request.env["HTTP_USER_AGENT"] =~ /iPhone/
+    request.env["HTTP_USER_AGENT"].try do |ua|
+      ua =~ /iPhone/ || ua =~ /iPad/ || ua =~ /iPod/
+    end
   end
 end
